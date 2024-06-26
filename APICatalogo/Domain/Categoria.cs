21806f95ad7ab8ae8122733c9773e94bad41e5bd
Stanislaw Cruz <stanycruz@outlook.com>
2024-06-26 19:19:39 -0300

    Definindo o relacionamento entre as entidades

--- APICatalogo/Domain/Categoria.cs
+++ APICatalogo/Domain/Categoria.cs
@@ -1,8 +1,16 @@
+using System.Collections.ObjectModel;
+
 namespace APICatalogo.Domain;
 
 public class Categoria
 {
+    public Categoria()
+    {
+        Produtos = new Collection<Produto>();
+    }
+
     public int CategoriaId { get; set; }
     public string? Nome { get; set; }
     public string? ImageUrl { get; set; }
+    public ICollection<Produto>? Produtos { get; set; }
 }
