b278337b7d66a4796984caecca0b2caaa4db89a5
Stanislaw Cruz <stanycruz@outlook.com>
2024-06-26 16:17:04 -0300

    Criando o modelo de domínio

---
+++ APICatalogo/Domain/Categoria.cs
@@ -0,0 +1,8 @@
+namespace APICatalogo.Domain;
+
+public class Categoria
+{
+    public int CategoriaId { get; set; }
+    public string? Nome { get; set; }
+    public string? ImageUrl { get; set; }
+}
