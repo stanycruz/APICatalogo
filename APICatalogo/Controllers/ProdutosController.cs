84f708d599ac1832dd778494dbf89462fc9608ae
Stanislaw Cruz <stanycruz@outlook.com>
2024-07-12 18:27:11 -0300

    ProdutosController - DELETE

--- APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -1,16 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using APICatalogo.Context;
 using APICatalogo.Domain;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 
 namespace APICatalogo.Controllers
 {
+    [ApiController]
     [Route("[controller]")]
     public class ProdutosController : ControllerBase
     {
@@ -68,6 +63,22 @@ namespace APICatalogo.Controllers
             }
 
             _context.Entry(produto).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok(produto);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+
+            if (produto is null)
+            {
+                return NotFound("Produto não localizado...");
+            }
+
+            _context.Produtos.Remove(produto);
             _context.SaveChanges();
 
             return Ok(produto);
