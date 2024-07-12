efbb4ef9e7c3abeea7894e447928eb5c8d49ae93
Stanislaw Cruz <stanycruz@outlook.com>
2024-07-12 18:09:40 -0300

    ProdutosController - POST

--- APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -31,7 +31,7 @@ namespace APICatalogo.Controllers
             return produtos;
         }
 
-        [HttpGet("{id:int}")]
+        [HttpGet("{id:int}", Name = "ObterProduto")]
         public ActionResult<Produto> Get(int id)
         {
             var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
@@ -40,6 +40,22 @@ namespace APICatalogo.Controllers
                 return NotFound("Produto não encontrado...");
             }
             return produto;
+        }
+
+        [HttpPost]
+        public ActionResult Post(Produto produto)
+        {
+            if (produto is null)
+                return BadRequest();
+
+            _context.Produtos.Add(produto);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult(
+                "ObterProduto",
+                new { id = produto.ProdutoId },
+                produto
+            );
         }
     }
 }
